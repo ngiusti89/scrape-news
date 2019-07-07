66a5369b0c4f8feb511d7c175d92ccd766610313
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-07-06 19:01:02 -0700

    scraping, empty results

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
+
+console.log("\n***********************************\n" +
+    "scraping\n" +
+    "\n***********************************\n");
+
+axios.get("http://www.npr.org/sections/news/").then(function (response) {
+    var $ = cheerio.load(response.data);
+    var results = [];
+
+    $("article").each(function (i, element) {
+        var title = $(element).children("item-info").children(".title").children("a").text();
+
+        var link = $(element).children("item-info").find("h2.title").find("a").attr("href");
+
+        results.push({
+            title: title,
+            link: link
+        });
+    });
+
+    console.log(results);
+
+});
