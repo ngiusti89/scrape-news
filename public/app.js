ac21b9e1462d14d8b46994921251088320d66fc4
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-07-14 16:51:50 -0700

    updates

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 $.getJSON("/articles", function (data) {
     for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
         $("#articles").append();
     }
 });
